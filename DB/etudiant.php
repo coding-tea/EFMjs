5c2e671d6eac0ce3ccf3a3491dd499d4dbb9e392
coding-tea <prgaming.ismail@gmail.com>
2024-05-23 17:07:00 +0100

    update method

--- DB/etudiant.php
+++ DB/etudiant.php
@@ -28,29 +28,24 @@ class Etudiant
     public static function add($id, $civilite, $nom, $prenom, $email, $photo)
     {
         $image = $photo;
-        //Stores the filename as it was on the client computer.
         $imagename = $_FILES['photo']['name'];
-        //Stores the filetype e.g image/jpeg
         $imagetype = $_FILES['photo']['type'];
-        //Stores any error codes from the upload.
         $imageerror = $_FILES['photo']['error'];
-        //Stores the tempname as it is given by the host when uploaded.
         $imagetemp = $_FILES['photo']['tmp_name'];
 
-        //The path you wish to upload the image to
         $imagePath = "images/";
 
-        if (is_uploaded_file($imagetemp)) {
-        move_uploaded_file($imagetemp, $imagePath . $imagename);
+        if (is_uploaded_file($imagetemp))
+            move_uploaded_file($imagetemp, $imagePath . $imagename);
         $stmt = self::db()->prepare("insert into etudiants values(?, ?, ?,  ?, ?, ?, ?)");
         $flag = $stmt->execute([$id, $civilite, $nom, $prenom, $email, "images/" . $imagePath . $imagename]);
         return json_encode($stmt->fetch(PDO::FETCH_OBJ));
     }
 
-    public static function update($id)
+    public static function update($id, $civilite, $nom, $prenom, $email, $photo)
     {
-        $stmt = self::db()->prepare("select * from etudiants where id = ?");
-        $flag = $stmt->execute($id);
+        $stmt = self::db()->prepare("update etudiants set civilite=?, nom = ?, prenom=?, email=? where id = ?");
+        $flag = $stmt->execute([$civilite, $nom, $prenom, $email, $id]);
         return json_encode($stmt->fetch(PDO::FETCH_OBJ));
     }
 
